1db8cebd390f9d69e10c51643db0ea2bca2074da
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-01 23:17:14 -0300

    feat: created ecommerce API

---
+++ lib/ecommerceApi-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from "aws-cdk-lib"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as apigateway from "aws-cdk-lib/aws-apigateway"
+import * as cwlogs from "aws-cdk-lib/aws-logs"
+import { Construct } from 'constructs'
+
+interface ECommerceApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
+export class EcommerceApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ECommerceApiStackProps){
+    super(scope, id, props)
+
+    const api = new apigateway.RestApi(this, "ECommerceApi", {
+      restApiName: "ECommerceApi",
+    
+    })
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+
+    // Route /products
+    const productsResource = api.root.addResource("products")
+    productsResource.addMethod("GET", productsFetchIntegration)
+
+
+  }
+}
