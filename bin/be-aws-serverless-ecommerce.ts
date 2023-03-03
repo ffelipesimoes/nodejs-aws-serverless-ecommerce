365e992032b4bf1bb12dc7c88256635480bbe52d
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-03 09:59:12 -0300

    chore: updating principal stack file

--- bin/be-aws-serverless-ecommerce.ts
+++ bin/be-aws-serverless-ecommerce.ts
@@ -1,21 +1,30 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { BeAwsServerlessEcommerceStack } from '../lib/be-aws-serverless-ecommerce-stack';
+import { ProductsAppStack } from '../lib/productsApp-stack';
+import { EcommerceApiStack } from '../lib/ecommerceApi-stack';
 
 const app = new cdk.App();
-new BeAwsServerlessEcommerceStack(app, 'BeAwsServerlessEcommerceStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const env: cdk.Environment = {
+  account: "645543366511",
+  region: "us-east-1"
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const tags = {
+  cost: "ECommerce",
+  team: "DigitalRegistro"
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+})
+
+const eCommerceApiStack = new EcommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  tags: tags,
+  env: env,
+})
+
+eCommerceApiStack.addDependency(productsAppStack)
