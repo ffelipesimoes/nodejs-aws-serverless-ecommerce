ec55c2b0f89b628d640f9bae0cba175d937e1df3
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-09 00:07:04 -0300

    feat: update products fetch function

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,4 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { ProductRepository } from '/opt/nodejs/productsLayer';
+import { DynamoDB } from "aws-sdk"
+
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export async function handler(
   event:APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
@@ -11,29 +18,38 @@ export async function handler(
 
     if (event.resource === "/products") {
       if (method === 'GET') {
-        console.log('get')
+        console.log('GET /products')
+
+        const products = await productRepository.getAllProducts()
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(products)
+        }
       }
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: "GET products - OK"
-        })
-      }
     } else if (event.resource === "/products/{id}") {
       const productId = event.pathParameters!.id as string
       console.log(`GET /products/${productId}`)
 
-      return {
-        statusCode: 200,
-        body: `GET /products/${productId}`
-        }
+      try {
+        const product = await productRepository.getProductById(productId)
+        return {
+          statusCode: 200,
+          body: JSON.stringify(product)
+          }
+      } catch (error) {
+        console.error((<Error>error).message)
+        return {
+          statusCode: 404,
+          body: (<Error>error).message
+          }
       }
-
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "Bad request"
-      })
-    }
+  }
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Bad request"
+    })
+  }
 }
