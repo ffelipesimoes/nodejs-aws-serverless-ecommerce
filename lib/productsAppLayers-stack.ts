9daefa9f9ba31c72d9a4834a980a56476c8a213e
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-10 07:27:18 -0300

    refact: removing class attributes

--- lib/productsAppLayers-stack.ts
+++ lib/productsAppLayers-stack.ts
@@ -4,12 +4,11 @@ import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as ssm from "aws-cdk-lib/aws-ssm"
 
 export class ProductsAppLayersStack extends cdk.Stack {
-   readonly productsLayers: lambda.LayerVersion
 
    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props)
 
-      this.productsLayers = new lambda.LayerVersion(this, "ProductsLayer", {
+      const productsLayers = new lambda.LayerVersion(this, "ProductsLayer", {
          code: lambda.Code.fromAsset('lambda/products/layers/productsLayer'),
          compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
          layerVersionName: "ProductsLayer",
@@ -17,7 +16,7 @@ export class ProductsAppLayersStack extends cdk.Stack {
       })
       new ssm.StringParameter(this, "ProductsLayerVersionArn", {
          parameterName: "ProductsLayerVersionArn",
-         stringValue: this.productsLayers.layerVersionArn
+         stringValue: productsLayers.layerVersionArn
       })
    }
 }
