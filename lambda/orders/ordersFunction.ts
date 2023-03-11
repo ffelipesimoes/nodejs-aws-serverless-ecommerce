599f3e6a0c33ef244f7d527ab076a58f1cb53fb6
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-11 14:37:15 -0300

    feat: created aux convertToOrderResponse function

--- lambda/orders/ordersFunction.ts
+++ lambda/orders/ordersFunction.ts
@@ -4,7 +4,7 @@ import { Product, ProductRepository } from '/opt/nodejs/productsLayer'
 
 import * as AWSXRay from "aws-xray-sdk"
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
-import { OrderProductResponse, OrderRequest } from '/opt/nodejs/ordersApiLayer'
+import { CarrierType, OrderProductResponse, OrderRequest, OrderResponse, PaymentType, ShippingType } from '/opt/nodejs/ordersApiLayer'
 
 AWSXRay.captureAWS(require("aws-sdk"))
 
@@ -55,6 +55,33 @@ export async function handler(
       statusCode: 400,
       body: "bad request"
     }
+}
+
+function convertToOrderResponse(order: Order): OrderResponse {
+  const orderProducts: OrderProductResponse[] = []
+  order.products.forEach((product) => {
+    orderProducts.push({
+      code: product.code,
+      price: product.price
+    })
+  })
+
+  const orderResponse: OrderResponse = {
+    email: order.pk,
+    id: order.sk!,
+    createdAt: order.createdAt!,
+    products: orderProducts,
+    billing: {
+      payment: order.billing.payment as PaymentType,
+      totalPrice: order.billing.totalPrice
+    },
+    shipping: {
+      type: order.shipping.type as ShippingType,
+      carrier: order.shipping.carrier as CarrierType
+    }
+  }
+
+  return orderResponse
 }
 
 function buildOrder(orderRequest: OrderRequest, products: Product[]): Order {
