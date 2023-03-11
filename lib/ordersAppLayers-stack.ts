f5ef4bb0747687f49b960b7b3c81891011be7786
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-11 14:09:15 -0300

    feat: created orderApi stack

--- lib/ordersAppLayers-stack.ts
+++ lib/ordersAppLayers-stack.ts
@@ -17,5 +17,16 @@ export class OrdersAppLayersStack extends cdk.Stack {
       parameterName: "OrdersLayerVersionArn",
       stringValue: ordersLayer.layerVersionArn
    })
+
+   const ordersApiLayer = new lambda.LayerVersion(this, "OrdersApiLayer", {
+    code: lambda.Code.fromAsset('lambda/orders/layers/ordersApiLayer'),
+    compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
+    layerVersionName: "OrdersApiLayer",
+    removalPolicy: cdk.RemovalPolicy.RETAIN
+ })
+ new ssm.StringParameter(this, "OrdersApiLayerVersionArn", {
+    parameterName: "OrdersApiLayerVersionArn",
+    stringValue: ordersApiLayer.layerVersionArn
+ })
   }
 }
