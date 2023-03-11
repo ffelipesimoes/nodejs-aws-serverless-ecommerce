764297e7fff0a5c0ed1e693b9e4cf893dee88602
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-11 12:07:38 -0300

    feat: added ordersApp and ordersAppLayer into main stack

--- bin/be-aws-serverless-ecommerce.ts
+++ bin/be-aws-serverless-ecommerce.ts
@@ -5,6 +5,8 @@ import { ProductsAppStack } from '../lib/productsApp-stack';
 import { EcommerceApiStack } from '../lib/ecommerceApi-stack';
 import { ProductsAppLayersStack } from '../lib/productsAppLayers-stack';
 import { EventsDdbStack } from '../lib/eventsDdb-stack';
+import { OrdersAppLayersStack } from '../lib/ordersAppLayers-stack';
+import { OrdersAppStack } from '../lib/ordersApp-stack';
 
 const app = new cdk.App();
 
@@ -36,11 +38,27 @@ const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
 productsAppStack.addDependency(productsAppLayersStack)
 productsAppStack.addDependency(eventsDdbStack)
 
+const ordersAppLayersStack = new OrdersAppLayersStack(app, "OrdersAppLayers", {
+  tags: tags,
+  env: env
+})
+
+const ordersAppStack = new OrdersAppStack(app, "OrdersApp", {
+  tags: tags,
+  env: env,
+  productsDdb: productsAppStack.productsDdb
+})
+
+ordersAppStack.addDependency(productsAppStack)
+ordersAppStack.addDependency(ordersAppStack)
+
 const eCommerceApiStack = new EcommerceApiStack(app, "ECommerceApi", {
   productsFetchHandler: productsAppStack.productsFetchHandler,
   productsAdminHandler: productsAppStack.productsAdminHandler,
+  ordersHandler: ordersAppStack.ordersHandler,
   tags: tags,
   env: env,
 })
 
 eCommerceApiStack.addDependency(productsAppStack)
+eCommerceApiStack.addDependency(ordersAppStack)
