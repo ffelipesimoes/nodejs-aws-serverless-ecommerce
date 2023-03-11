b3418f637387b428117bb381ad4de6461126d524
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-11 10:40:29 -0300

    feat: created table orders

---
+++ lib/ordersApp-stack.ts
@@ -0,0 +1,34 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
+import * as ssm from "aws-cdk-lib/aws-ssm"
+
+import { Construct } from 'constructs'
+
+interface OrdersAppStackProps extends cdk.StackProps {
+  productsDdb: dynamodb.Table
+}
+
+export class OrdersAppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: OrdersAppStackProps){
+    super(scope, id, props)
+
+    //This creates a table on dynamodb
+     const ordersDdb = new dynamodb.Table(this, "OrdersDdb", {
+      tableName: "orders",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: "pk",
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey: {
+        name: "sk",
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1
+    })
+  }
+}
