f3482b96236e2c42117a28256380c4987794160a
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-11 13:31:40 -0300

    feat: created orderFunction lambda properly

---
+++ lambda/orders/ordersFunction.ts
@@ -0,0 +1,57 @@
+import { DynamoDB } from 'aws-sdk'
+import { OrderRepository } from '/opt/nodejs/ordersLayer' 
+import { ProductRepository } from '/opt/nodejs/productsLayer'
+
+import * as AWSXRay from "aws-xray-sdk"
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
+
+AWSXRay.captureAWS(require("aws-sdk"))
+
+const ordersDdb = process.env.ORDERS_DDB!
+const productsDdb = process.env.PRODUCTS_DDB!
+
+const ddbClient = new DynamoDB.DocumentClient()
+
+const orderRepository = new OrderRepository(ddbClient, ordersDdb)
+const productRepository = new ProductRepository(ddbClient, productsDdb)
+
+
+export async function handler(
+  event:APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+
+    const method = event.httpMethod
+    const apiRequestId = event.requestContext.requestId
+    const lambdaRequestId = context.awsRequestId
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - lambda RequestId: ${lambdaRequestId}`)
+
+    if (method === "GET"){
+      if(event.queryStringParameters) {
+        const email = event.queryStringParameters!.email
+        const orderId = event.queryStringParameters!.orderId
+        if(email) {
+          if(orderId){
+            //get one order from an user
+          } else {
+            //get all orders from an user
+          }
+        }
+      } else {
+        //get all orders
+      }
+
+    } else if (method === "POST"){
+      console.log("/POST Order")
+
+    } else if (method === "DELETE"){
+      console.log("/DELETE Order")   
+      const email = event.queryStringParameters!.email
+      const orderId = event.queryStringParameters!.orderId   
+
+    }
+
+    return{
+      statusCode: 400,
+      body: "bad request"
+    }
+}
