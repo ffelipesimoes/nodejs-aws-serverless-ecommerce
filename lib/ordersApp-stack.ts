49c89c5a580b84f3ed36c846a77427edb9c30a8c
Felipe Simões <ffelipesimoes@gmail.com>
2023-03-16 19:26:39 -0300

    feat: added OrderEvents layer

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -44,6 +44,10 @@ export class OrdersAppStack extends cdk.Stack {
     const ordersApiLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrdersApiLayerVersionArn" )
     const ordersApiLayer = lambda.LayerVersion.fromLayerVersionArn(this, "OrdersApiLayerVersionArn", ordersApiLayerArn)
 
+    //Order Event Layer
+    const orderEventsLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrderEventsLayerVersionArn" )
+    const orderEventsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "OrderEventsLayerVersionArn", orderEventsLayerArn)
+
     //Products Layer
     const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn" )
     const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
@@ -69,7 +73,7 @@ export class OrdersAppStack extends cdk.Stack {
            ORDERS_DDB: ordersDdb.tableName,
            ORDER_EVENTS_TOPIC_ARN: ordersTopic.topicArn
          },
-         layers: [ordersLayer, productsLayer, ordersApiLayer],
+         layers: [ordersLayer, productsLayer, ordersApiLayer, orderEventsLayer],
          tracing: lambda.Tracing.ACTIVE,
          insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
        })
