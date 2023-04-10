3d428b4fc1b3d76e1603dc42ea626cd012a8a178
Felipe Simões <ffelipesimoes@gmail.com>
2023-04-09 22:01:00 -0300

    feat: created order-events queue

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -6,6 +6,9 @@ import * as ssm from "aws-cdk-lib/aws-ssm"
 import * as sns from "aws-cdk-lib/aws-sns"
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions"
 import * as iam from "aws-cdk-lib/aws-iam"
+import * as sqs from "aws-cdk-lib/aws-sqs"
+import * as lambdaEventSource from "aws-cdk-lib/aws-lambda-event-sources"
+
 
 
 import { Construct } from 'constructs'
@@ -143,5 +146,12 @@ export class OrdersAppStack extends cdk.Stack {
         })
       }
     }))
+
+    const orderEventsQueue = new sqs.Queue(this, "OrderEventsQueue", {
+      queueName: "order-events"
+    })
+
+    ordersTopic.addSubscription(new subs.SqsSubscription(orderEventsQueue))
+
   }
 }
