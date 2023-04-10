125d0c8cb49c71643d4e235063b638fe97686394
Felipe Simões <ffelipesimoes@gmail.com>
2023-04-09 21:51:12 -0300

    feat: billing function and SNS filters

--- lambda/orders/ordersFunction.ts
+++ lambda/orders/ordersFunction.ts
@@ -148,7 +148,13 @@ function sendOrderEvent(order: Order, eventType: OrderEventType, lambdaRequestId
 
   return snsClient.publish({
     TopicArn: orderEventsTopicArn,
-    Message: JSON.stringify(envelope)
+    Message: JSON.stringify(envelope),
+    MessageAttributes: {
+      eventType: {
+        DataType: "String",
+        StringValue: eventType
+      }
+    }
   }).promise()
 }
 
