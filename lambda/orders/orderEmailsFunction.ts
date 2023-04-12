fae422f39e6f81afee700b35ed497dc3a41d6c70
Felipe Simões <ffelipesimoes@gmail.com>
2023-04-12 09:44:39 -0300

    feat: added send email functionality on order function

--- lambda/orders/orderEmailsFunction.ts
+++ lambda/orders/orderEmailsFunction.ts
@@ -1,13 +1,48 @@
-import { Context , SQSEvent} from "aws-lambda"
+import { Context , SNSMessage, SQSEvent} from "aws-lambda"
+import { AWSError, SES } from 'aws-sdk'
 import * as AWSRay from "aws-xray-sdk"
+import { Envelope, OrderEvent } from '/opt/nodejs/orderEventsLayer'
+import { PromiseResult } from 'aws-sdk/lib/request'
 
 AWSRay.captureAWS(require("aws-sdk"))
 
+const sesClient = new SES()
+
 export async function handler(event: SQSEvent, context: Context): Promise<void> {
+  const promises : Promise<PromiseResult<SES.SendEmailResponse, AWSError>>[]= []
+
+  
   event.Records.forEach((record) => {
-    console.log(record)
-    const body = JSON.parse(record.body)
-    console.log(body)
+    const body = JSON.parse(record.body) as SNSMessage
+    promises.push(sendOrderEmail(body))
   })
+
+  await Promise.all(promises)
   return
 }
+
+function sendOrderEmail(body: SNSMessage){
+  const envelope = JSON.parse(body.Message) as Envelope
+  const event = JSON.parse(envelope.data) as OrderEvent
+
+  return sesClient.sendEmail({
+    Destination: {
+      ToAddresses: [event.email]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `Recebemos seu pedido de numero ${event.orderId} no valor de R$ ${event.billing.totalPrice}`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Recebemos seu pedido!",
+      },
+    },
+    Source: "ffelipesimoes@gmail.com",
+    ReplyToAddresses: ["ffelipesimoes@gmail.com"],
+  }).promise()
+}
+  
