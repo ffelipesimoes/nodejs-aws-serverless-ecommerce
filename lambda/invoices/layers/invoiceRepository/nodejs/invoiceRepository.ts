418e04118ef698e8de7bbdb922cada654b90e346
Felipe Simões <ffelipesimoes@gmail.com>
2023-04-22 17:35:23 -0300

    feat: added invoiceRepository

---
+++ lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
@@ -0,0 +1,39 @@
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+
+export interface InvoiceFile {
+  customerName: string;
+  invoiceNumber: string;
+  totalValue: number;
+  productId: string;
+  quantity: number;
+}
+
+export interface Invoice {
+  pk: string;
+  sk: string;
+  totalValue: number;
+  productId: string;
+  quantity: number;
+  transactionId: string;
+  ttl: number;
+  createdAt: string;
+}
+
+export class InvoiceRepository {
+  private ddbClient: DocumentClient;
+  private invoiceDdb: string;
+
+  constructor(ddbClient: DocumentClient, invoiceDdb: string) {
+    this.ddbClient = ddbClient;
+    this.invoiceDdb = invoiceDdb;
+  }
+
+  async create(invoice: Invoice): Promise<Invoice> {
+    await this.ddbClient.put({
+        TableName: this.invoiceDdb,
+        Item: invoice,
+      })
+      .promise();
+      return invoice
+  }
+}
