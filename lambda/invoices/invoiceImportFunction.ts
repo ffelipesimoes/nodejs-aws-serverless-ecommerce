f51a178e426c002c605ea5fc75b64311422349e7
Felipe Simões <ffelipesimoes@gmail.com>
2023-04-22 18:44:48 -0300

    feat: added invoice Import function

---
+++ lambda/invoices/invoiceImportFunction.ts
@@ -0,0 +1,89 @@
+import { Context, S3Event, S3EventRecord } from 'aws-lambda';
+import { ApiGatewayManagementApi, DynamoDB, S3 } from 'aws-sdk';
+import * as AWSXRay from 'aws-xray-sdk';
+import { InvoiceTransactionRepository, InvoiceTransactionStatus } from '/opt/nodejs/invoiceTransaction';
+import { InvoiceWSService } from '/opt/nodejs/invoiceWSConnection';
+import { InvoiceFile, InvoiceRepository } from '/opt/nodejs/invoiceRepository';
+
+AWSXRay.captureAWS(require('aws-sdk'));
+
+const invoicesDdb = process.env.INVOICE_DDB!
+const invoicesWsApiEndpoint = process.env.INVOICE_WSAPI_ENDPOINT!.substring(6)
+
+const s3Client = new S3()
+const ddbClient = new DynamoDB.DocumentClient()
+const apigwManagementApi = new ApiGatewayManagementApi({
+  endpoint: invoicesWsApiEndpoint
+})
+
+const invoiceTransactionRepository = new InvoiceTransactionRepository(ddbClient, invoicesDdb)
+const invoiceWSService = new InvoiceWSService(apigwManagementApi)
+const invoiceRepository = new InvoiceRepository(ddbClient, invoicesDdb)
+
+export async function handler(event: S3Event, context: Context): Promise<void> {
+  console.log('Event: ', event);
+  console.log('Context: ', context);
+
+   const promises: Promise<void>[] = []
+
+   event.Records.forEach(record => {
+    promises.push(processRecord(record))
+  })
+
+  await Promise.all(promises)
+ 
+  }
+
+async function processRecord(record: S3EventRecord){
+
+  const key = record.s3.object.key
+
+  try {
+    const invoiceTransaction = await invoiceTransactionRepository.getInvoiceTransaction(key)
+
+    if(invoiceTransaction.transactionStatus === InvoiceTransactionStatus.GENERATED){
+
+      await Promise.all([invoiceWSService.sendInvoiceStatus(key, invoiceTransaction.connectionId, InvoiceTransactionStatus.RECEIVED),
+      invoiceTransactionRepository.updateInvoiceTransaction(key, InvoiceTransactionStatus.RECEIVED)])
+
+    } else {
+      await invoiceWSService.sendInvoiceStatus(key, invoiceTransaction.connectionId, invoiceTransaction.transactionStatus)
+      console.error(`Non valid transaction status`)
+      return
+    }
+
+    const object = await s3Client.getObject({
+      Key: key,
+      Bucket: record.s3.bucket.name
+    }).promise()
+
+    const invoice = JSON.parse(object.Body!.toString('utf-8')) as InvoiceFile
+
+    const createInvoicePromise =  invoiceRepository.create({
+      pk: `#invoice_${invoice.customerName}`,
+      sk: invoice.invoiceNumber,
+      ttl: 0,
+      totalValue: invoice.totalValue,
+      productId: invoice.productId,
+      quantity: invoice.quantity,
+      transactionId: key,
+      createdAt: Date.now()
+    })
+
+    const deleteObjectPromise = s3Client.deleteObject({
+      Key: key,
+      Bucket: record.s3.bucket.name
+    }).promise()
+
+    const updateInvoicePromise = invoiceTransactionRepository.updateInvoiceTransaction(key, InvoiceTransactionStatus.PROCESSED)
+    const sendStatusPromise = invoiceWSService.sendInvoiceStatus(key, invoiceTransaction.connectionId, InvoiceTransactionStatus.PROCESSED)
+
+    await Promise.all([createInvoicePromise, deleteObjectPromise, updateInvoicePromise, sendStatusPromise])
+
+    
+  } catch (error) {
+    console.log((<Error>error).message)
+  }
+
+}
+
--- lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
+++ lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
@@ -16,7 +16,7 @@ export interface Invoice {
   quantity: number;
   transactionId: string;
   ttl: number;
-  createdAt: string;
+  createdAt: number;
 }
 
 export class InvoiceRepository {
