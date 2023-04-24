1ef76e0c580bf306ce4887fdf66c3ab835c2ce41
Felipe Simões <ffelipesimoes@gmail.com>
2023-04-23 22:22:15 -0300

    feat: added audit dependences on others stacks

--- lib/invoiceWSApi-stack.ts
+++ lib/invoiceWSApi-stack.ts
@@ -10,10 +10,13 @@ import * as s3n from "aws-cdk-lib/aws-s3-notifications"
 import * as ssm from "aws-cdk-lib/aws-ssm"
 import * as sqs from "aws-cdk-lib/aws-sqs"
 import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources"
+import * as event from "aws-cdk-lib/aws-events"
+
 import { Construct } from 'constructs'
 
 interface InvoiceWSApiStackProps extends cdk.StackProps {
   eventsDdb: dynamodb.Table
+  auditBus: event.EventBus
 }
 
 export class InvoiceWSApiStack extends cdk.Stack {
@@ -171,10 +174,14 @@ export class InvoiceWSApiStack extends cdk.Stack {
       tracing: lambda.Tracing.ACTIVE,
       environment: {
         INVOICE_DDB: invoicesDdb.tableName,
-        INVOICE_WSAPI_ENDPOINT: wsApiEndpoint
+        INVOICE_WSAPI_ENDPOINT: wsApiEndpoint,
+        AUDIT_BUS_NAME: props.auditBus.eventBusName
+
       }
     })
     invoicesDdb.grantReadWriteData(invoiceImportHandler)
+    props.auditBus.grantPutEventsTo(invoiceImportHandler)
+
 
     bucket.addEventNotification(
       s3.EventType.OBJECT_CREATED_PUT,
@@ -246,12 +253,16 @@ export class InvoiceWSApiStack extends cdk.Stack {
       environment: {
         EVENTS_DDB: props.eventsDdb.tableName,
         INVOICE_WSAPI_ENDPOINT: wsApiEndpoint,
+        AUDIT_BUS_NAME: props.auditBus.eventBusName
+
       },
       tracing: lambda.Tracing.ACTIVE,
       layers: [invoiceWSConnectionLayer]
     })
 
     webSocketApi.grantManageConnections(invoiceEventsHandler)
+    props.auditBus.grantPutEventsTo(invoiceEventsHandler)
+
 
     const eventsDdbPolicy = new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -8,12 +8,14 @@ import * as subs from "aws-cdk-lib/aws-sns-subscriptions"
 import * as iam from "aws-cdk-lib/aws-iam"
 import * as sqs from "aws-cdk-lib/aws-sqs"
 import * as lambdaEventSource from "aws-cdk-lib/aws-lambda-event-sources"
+import * as event from "aws-cdk-lib/aws-events"
 
 import { Construct } from 'constructs'
 
 interface OrdersAppStackProps extends cdk.StackProps {
   productsDdb: dynamodb.Table
   eventsDdb: dynamodb.Table
+  auditBus: event.EventBus
 }
 
 export class OrdersAppStack extends cdk.Stack {
@@ -80,7 +82,8 @@ export class OrdersAppStack extends cdk.Stack {
          environment: {
            PRODUCTS_DDB: props.productsDdb.tableName,
            ORDERS_DDB: ordersDdb.tableName,
-           ORDER_EVENTS_TOPIC_ARN: ordersTopic.topicArn
+           ORDER_EVENTS_TOPIC_ARN: ordersTopic.topicArn,
+           AUDIT_BUS_NAME: props.auditBus.eventBusName
          },
          layers: [ordersLayer, productsLayer, ordersApiLayer, orderEventsLayer],
          tracing: lambda.Tracing.ACTIVE,
@@ -90,6 +93,7 @@ export class OrdersAppStack extends cdk.Stack {
     ordersDdb.grantReadWriteData(this.ordersHandler)
     props.productsDdb.grantReadData(this.ordersHandler)
     ordersTopic.grantPublish(this.ordersHandler)
+    props.auditBus.grantPutEventsTo(this.ordersHandler)
 
     const orderEventsHandler = new lambdaNodeJS.NodejsFunction(this, "OrderEventsFunction", {
       functionName: "OrderEventsFunction",
