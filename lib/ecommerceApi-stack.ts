0b6f8598eb5cb20d77a621db4e5024cdd853adde
Felipe Simões <ffelipesimoes@gmail.com>
2023-05-19 12:08:07 -0300

    🚀 feat(ecommerceApi-stack.ts): add IAM policy to allow AdminGetUser action on Cognito user pool to the products admin handler role to enable admin user management.

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -4,6 +4,7 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import * as cwlogs from "aws-cdk-lib/aws-logs"
 import * as cognito from "aws-cdk-lib/aws-cognito"
 import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as iam from "aws-cdk-lib/aws-iam"
 import { Construct } from 'constructs'
 
 interface ECommerceApiStackProps extends cdk.StackProps {
@@ -47,6 +48,18 @@ export class EcommerceApiStack extends cdk.Stack {
     })
 
     this.createCognitoAuth()
+
+    const adminUserPolicyStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["cognito-idp:AdminGetUser"],
+      resources: [this.adminPool.userPoolArn]
+    })
+
+    const adminUserPolicy = new iam.Policy(this, "AdminGetUserPolicy", {
+      statements: [adminUserPolicyStatement]
+    })
+
+    adminUserPolicy.attachToRole(<iam.Role>props.productsAdminHandler.role)
 
     this.createProductsService(props, api)
 
